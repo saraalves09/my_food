b634f08be7ac42f80089451d51d0fbbeead5f052
aluno <gurgaczzhellen@gmail.com>
2022-03-23 20:55:10 -0300

    add

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 
+const LISTA_DE_PESSOAS = [
+  {
+    id: 1,
+    nome:'Hellen',
+    cpf: '12345678974',
+    foto: '../assets/img/1.jpeg'
+  },
+  {
+    id: 2,
+    nome:'Ana',
+    cpf: '15247899999',
+    foto: '../assets/img/2.jpeg'
+  }
+];
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -7,9 +22,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  listaPessoas = LISTA_DE_PESSOAS;
   constructor() { }
 
   ngOnInit(): void {
+
   }
 
 }
