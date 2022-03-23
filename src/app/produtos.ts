4c59a579f08696f4bf70cde9dc05f22070d74c38
aluno <Aluno@DOSO027384.educacional.up>
2022-03-23 19:49:12 -0300

    add

--- src/app/produtos.ts
+++ src/app/produtos.ts
@@ -3,18 +3,18 @@ export const Produtos = [
         id: 1, 
         nome: 'Produto 1',
         desc: 'desc 1',
-        foto: '../assets/img/i1.jpeg'
+        foto: '../assets/img/1.jpeg'
     },
     {
         id: 2, 
         nome: 'Produto 2',
         desc: 'desc 2',
-        foto: '../assets/img/i2.jpeg'
+        foto: '../assets/img/2.jpeg'
     },
     {
         id: 3, 
         nome: 'Produto 3',
         desc: 'desc 3',
-        foto: '../assets/img/i3.jpeg'
+        foto: '../assets/img/3.jpeg'
     }
 ]
